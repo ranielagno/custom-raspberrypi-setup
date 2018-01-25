032f200968476877b8343dd645074dfc042ec026
ranielagno <ranielagno@gmail.com>
2018-01-25 20:59:25 +0800

    Modified digital-read.py

--- digital-read.py
+++ digital-read.py
@@ -2,26 +2,22 @@ import time, sys
 import RPi.GPIO as GPIO
 
 GPIO.setmode(GPIO.BOARD)
-GPIO.setup(14, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
-GPIO.setup(15, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
+GPIO.setup(16, GPIO.OUT)
+GPIO.setup(18, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
-def detectMediumGasConcentration(pin):
-    print('Medium Gas concentration detected!')
+def detectGas(pin):
+    print "High Gas concentration detected!"
     return
 
-def detectHighGasConcentration(pin):
-    print('High Gas concentration detected!)
-    return
 
-GPIO.add_event_detect(14, GPIO.RISING)
-GPIO.add_event_callback(14, detectMediumGasConcentration)
+GPIO.add_event_detect(18, GPIO.RISING)
+GPIO.add_event_callback(18, detectGas)
 
-GPIO.add_event_detect(15, GPIO.RISING)
-GPIO.add_event_callback(15, detectHighGasConcentration)
+GPIO.output(16, GPIO.HIGH)
 
 try:
     while True:
-        print('alive')
+        print "alive"
         time.sleep(0.5)
 except KeyboardInterrupt:
     GPIO.cleanup()
